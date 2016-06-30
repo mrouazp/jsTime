d9d8b8a33999fa6daf4f69811127feeb64c635a5
Ruslan <mroua@mail.ua>
2016-06-30 15:54:15 +0300

    01.07.16

--- js/time.js
+++ js/time.js
@@ -1,7 +1,7 @@
 /**
  * Created by Ruslan on 27.06.2016.
  */
-var oneSec = setInterval(myClock, 1000);
+/*var oneSec = setInterval(myClock, 1000);
 function myClock() {
     var t = new Date();
     document.getElementById("time").innerHTML = t.toLocaleTimeString();
@@ -27,7 +27,7 @@ function starTime() {
     if (h<10) h='0'+h;
     if (m<10) m='0'+m;
     if (s<10) s='0'+s;
-
+    
     if (init == 1) document.stopWatch.sec.value = h + ':' + m + ':' + s ;
     clocktimer = setTimeout('starTime()',10);
 }
@@ -41,4 +41,48 @@ function swButton() {
     else stpbutton.onclick = function() {
             clearTimeout(clocktimer);
         }
+}*/
+//Попытка - 2 
+var h1 = document.getElementsByTagName('h1')[0],
+    str = document.getElementById('str'),
+    stp = document.getElementById('stp'),
+    clr = document.getElementById('clr'),
+    sec = 0, min = 0, hours = 0,
+    sw;
+
+function starTime() {
+    sec++;
+    if (sec >= 60) {
+        sec = 0;
+        min++;
+        if (min >= 60) {
+            min = 0;
+            hours++;
+        }
+    }
+
+    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" +
+        (min ? (min > 9 ? min : "0" + min) : "00") + ":" + 
+        (sec > 9 ? sec : "0" + sec);
+
+    timer();
+}
+function timer() {
+    sw = setTimeout('starTime()', 1000);
+}
+timer();
+
+
+/* Старт */
+str.onclick = timer;
+
+/* Стоп */
+stp.onclick = function() {
+    clearTimeout(sw);
+}
+
+/* Кнопка сброса */
+clr.onclick = function() {
+    h1.textContent = "00:00:00";
+    sec = 0; min = 0; hours = 0;
 }
